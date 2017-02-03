ee6af677e3471909bd13bb76c2547b0d57cb43e0
Samuel Cox <crankydillo@gmail.com>
2017-02-03 10:16:17 -0600

    Setup arg parsing and subprocess execution.

--- src/main/scala/org/beeherd/riemann/monitor/App.scala
+++ src/main/scala/org/beeherd/riemann/monitor/App.scala
@@ -1,7 +1,21 @@
 package org.beeherd.riemann.monitor
 
+import scala.sys.process._
+
 object App {
+
   def main(args: Array[String]): Unit = {
-    println("hi")
+    val (riemannHost, riemannPort, riemannService, cmd) = args.toList match {
+      case h :: p :: s :: "--" :: rest => (h, p, s, rest)
+      case _ => 
+        printUsage()
+        sys.exit(1)
+    }
+    
+    Process(cmd).run()
+  }
+
+  def printUsage(): Unit = {
+    println("Usage: <cmd> <riemann-host> <riemann-port> <riemann-service-name> -- <cmd>")
   }
 }
